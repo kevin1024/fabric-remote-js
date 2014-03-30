3d6f27c88a5b026feeba897f2354e80693ad8b83
Kevin McCarthy <me@kevinmccarthy.org>
2014-03-30 12:18:53 -1000

    lets try to return a deferred

--- src/fabric-remote.js
+++ src/fabric-remote.js
@@ -1,6 +1,8 @@
 /*jslint node: true */
 'use strict';
 var http = require('http');
+var q = require('q');
+
 var FabricRemote = function(host, port, password) {
   this.host = host; 
   this.port = port; 
@@ -8,11 +10,20 @@ var FabricRemote = function(host, port, password) {
 };
 
 FabricRemote.prototype.request = function(method, path) {
+  var buffer = '';
+  var deferred = q.defer();
   var url = this.host + ':' + this.port + path;
-  http.request({
+  var res = http.request({
     path: url,
     method: method,
   });
+  res.on('data', function(data) {
+    buffer += data;
+  });
+  res.on('end', function() {
+    deferred.resolve(buffer);
+  });
+  return deferred;
 };
 
 FabricRemote.prototype.listTasks = function() {
