15640bd12a3020d9028f13b4ef6149c1f3a49f74
Kevin McCarthy <me@kevinmccarthy.org>
2014-03-30 13:05:24 -1000

    cant figure out why failure isnt getting called

--- spec/FabricRemoteJsSpec.js
+++ spec/FabricRemoteJsSpec.js
@@ -1,10 +1,42 @@
 var FabricRemote = require('../src/fabric-remote');
+var q = require('q');
+
+var MOCK_TASK_LIST = {
+      host_type: {
+        name: "host_type",
+        description: "returns the host type"
+      },
+      check_foo: {
+        name: "check_foo",
+        description: null
+      }
+}
+
 describe("FabricRemote", function() {
   var fr;
+  var deferred;
   beforeEach(function() {
     fr = new FabricRemote("http://localhost", 1234, "opensesame");
+    deferred = q.defer();
+    spyOn(fr,'request').andReturn(deferred.promise);
   });
   it("can list tasks", function() {
-    fr.listTasks();
+    fr.listTasks().then(function(data) {
+      expect(data).toEqual(MOCK_TASK_LIST);
+    });
+    deferred.resolve(MOCK_TASK_LIST);
+    expect(fr.request).toHaveBeenCalledWith('GET','/tasks');
+  });
+  it("can fail", function() {
+    var outputFuncs = {
+      success: function(data){},
+      failure: function(error){},
+    };
+    spyOn(outputFuncs, 'failure');
+    spyOn(outputFuncs, 'success');
+    fr.listTasks().then(outputFuncs.success, outputFuncs.failure);
+    deferred.reject('shit is broken');
+    expect(outputFuncs.success).not.toHaveBeenCalled();
+    expect(outputFuncs.failure).toHaveBeenCalled();
   });
 });
